64c664c34b43acf60ec62053ca0201c767ffcd7f
Zenon <fstxzero@gmail.com>
2024-11-30 19:02:27 +0700

    Update Xyber.lua

--- Xyber.lua
+++ Xyber.lua
@@ -1,1 +1,5 @@
---loader
+if game.PlaceId == 189707 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ITZenon/ITZ/refs/heads/main/NDS.lua"))()
+else
+    print("Game Not Supported")
+end
